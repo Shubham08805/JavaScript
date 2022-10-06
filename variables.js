80327f1e9ba2be0931fd1321e28cad1c779471ee
Shubham <shubhamjnv11508805@gmail.com>
2022-10-06 14:05:57 +0530

    initial commit

---
+++ variables.js
@@ -0,0 +1,32 @@
+// let, var, const
+// 4 ways to declare a variable 
+// Using let : block scope, cannot redeclare
+// var : Global scoped , redeclare , re-initialize, 
+// const : block scoped , cannot redeclare, cannot re-initialize
+// nothing ex: a=10;  
+
+
+// Hoisting
+a=10;
+console.log(a);
+var a;
+
+
+// console.log(b);
+// var b=10;
+// O/P: undefined
+
+
+// console.log(b);
+// let b=10;
+// O/P: ReferenceError: Cannot access 'b' before initialization
+
+console.log(3>2<0);
+console.log(4>5<=1);
+console.log(4==5==0);
+
+
+
+
+
+
